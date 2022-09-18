dc3e40a60e5fd6bf5b50ca0d472846130fa7922a
MuhammmadHannan <hannanraja1992@gmail.com>
2022-09-18 12:29:14 +0500

    crud operations all

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ app.get('/', (req, res) => {
         <h3>Endpoint3: </h3> <p>.src/operation/delete/id to delete the data of that id : Method : delete</p>
        <h3> Endpoint4: </h3> <p>.src/operation/update/id to update the data of id: Method : patch </p>
         <h3>Endpoint5: </h3> <p>.src/operation/add to add the data of new user : Method : Post</p>
+<h3>Endpoint6: </h3> <p>.src/operation/deleteall to delete all the data in DB : Method : delete </p>
+<h3>Endpoint7: </h3> <p>.src/operation/addAllaverage to print all the score of students : Method : get </p>
 
     `)
 })
--- router.js
+++ router.js
@@ -10,26 +10,98 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 router.use(bodyParser.json({ limit: '50mb', type: 'application/json' }));
 
 
-router.post('/add', async(req, res) => {
+router.post('/add', async (req, res) => {
+    db.find({ name: req.body.name }, async function (err, docs) {
+          if (docs.length >= 1) {
+            res.status(202).send('data already present for name : ' + req.body.name)
+        }
+        else {
+            await db.insert(req.body)
+            res.status(200).json({message: "Data added successfully"})
+        }
+    })
+})
+
+router.get('/read', async (req, res) => {
     try {
-        await db.insert(req.body)
-        res.status(200).json({'Message':"Record added successfully ${req.body.name}"})
+        await db.find({}, function (err, docs) {
+            x = docs;
+            res.status(200).json(x)
+        })
     }
     catch (err) {
-        res.status(500).send('an error is created ${err}')
+        res.status(500).json({ message: "Error in API" })
     }
+})
 
 
-    })
-
-router.delete('/delete/:idtodelete', async(req, res) => {
+router.get('/readone/:name', async (req, res) => {
     try {
-        await db.remove({ _id: req.params.idtodelete}, (err, datatodelete) => {
+        await db.find({ name: req.params.name }, function (err, docs) {
+            x = docs;
+            res.status(200).json(x);
+        });
+    }
+    catch (err) {
+        res.status(500).json({ message: "Error connecting database" })
+    }
+})
+
+
+router.patch('/update/:datatoupdate', async (req, res) => {
+    db.find({ _id: req.params.datatoupdate }, async(err,docs) => {
+        if (docs.length >= 1) {
+            try {
+                await db.update({ _id: req.params.datatoupdate }, req.body, { upsert: false }, (err, updateddata) => {
+                    if (err) {
+                        return
+                        res.status(500).json({ message: "error in db" })
+                    }
+                    if (updateddata) {
+                        res.send("data added successfully")
+                    }
+                    else {
+                        res.send("data not added ")
+                    }
+                })
+            }
+            catch (err) {
+                res.status(500).json({ message: "API is not working" })
+            }
+        }
+        else {
+            res.send("Data with this ID : " + req.params.datatoupdate + " is not found in the database")
+        }
+    });
+       
+})
+
+
+router.get('/averageCalulator', async(req, res) => {
+    var x = 0;
+    try {
+       await db.find({}, function (err, docs) {
+            docs.map((value) => {
+                x += value.score
+            })
+            res.status(200).json({ "the calculated average is " :  x / docs.length })
+        })
+    }
+    catch (err) {
+        res.status(500).send("An error occured : " + err)
+    }
+})
+
+
+
+router.delete('/delete/:idtodelete', async (req, res) => {
+    try {
+        await db.remove({ _id: req.params.idtodelete }, (err, datatodelete) => {
             if (err) {
                 res.status(200).json({ message: "error in database" })
             }
-            datatodelete? res.status(200).json({ message: "data delete" }) : res.status(500).json({ message: "Student with this id doesnot exists" })
-            
+            datatodelete ? res.status(200).json({ message: "data delete" }) : res.status(500).json({ message: "Student with this id doesnot exists" })
+
         })
     }
     catch (err) {
@@ -37,38 +109,17 @@ router.delete('/delete/:idtodelete', async(req, res) => {
     }
 });
 
-router.patch('/update/:datatoupdate', async (req, res) => {
+router.delete('/deleteall', async (req, res) => {
     try {
-        await db.update({_id: req.params.datatoupdate}, req.body, { upsert: false }, (err, updateddata) => {
+        await db.remove({}, {multi:true}, (err, datatodelete) => {
             if (err) {
-                return
-                res.status(500).json({ message: "error in db"})
+                res.status(200).json({ message: "error in database" })
             }
-            if (updateddata) {
-                res.send("data added successfully")
-            }
-            else {
-                res.send("data not added ")
-            }
+            datatodelete ? res.status(200).json({ message: "data delete" }) : res.status(500).json({ message: "No Student data present" })
         })
     }
     catch (err) {
-        res.status(500).json({message: "API is not working"})
+        res.status(500).json({ message: "error in API ENdpoint" })
     }
-})
-
-router.get('/read', (req, res) => {
-    db.find({}, function (err, docs) {
-        x = docs;
-        res.status(200).json(x);
-    });
-})
-
-router.get('/readone/:name', (req, res) => {
-    db.find({ lastname: req.params.name }, function (err, docs) {
-        x = docs;
-        res.status(200).json(x);
-    });
-})
-
+});
 module.exports = router; 
